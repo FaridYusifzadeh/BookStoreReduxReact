17c2712f83afd50c6fc2856cfb89bb1a587cfd2b
FaridYusifzadeh <fariddsx2015@gmail.com>
2020-02-03 12:26:25 +0400

    store

---
+++ src/reducers/index.js
@@ -0,0 +1,17 @@
+const initialState = {
+  books: []
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'BOOKS_LOADED':
+      return {
+        books: action.payload
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
 import { createStore } from 'redux';
-
 import reducer from './reducers';
 
 const store = createStore(reducer);
+
+console.log(store)
 
 export default store;
